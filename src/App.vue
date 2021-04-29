677a9a772393dffa2d5f04868a3d2980ea378742
islamdiab-stack <islamdiab967@gmail.com>
2021-04-29 15:42:04 +0200

    Import BlogHeader.vue component and add name component to components method

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
-  <div></div>
+  <BlogHeader />
 </template>
 
 <script>
+import BlogHeader from "./components/BlogHeader";
 export default {
   name: "App",
   components: {
-    
+    BlogHeader,
   },
 };
 </script>
 
 <style>
-#app {
+* {
+  padding: 0;
+  margin: 0;
+}
+body{
+  font-family: 'Lato', sans-serif;
+}
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
