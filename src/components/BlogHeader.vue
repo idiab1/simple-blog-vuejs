5645c61fdb27877398866c10cc6eab6cbc2e32da
islamdiab-stack <islamdiab967@gmail.com>
2021-04-29 15:40:10 +0200

    Create new component for header app where conains BlogHeader.vue three item for template, script, and style. i used language sass for styling sass is a preprocessor for css

---
+++ src/components/BlogHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="header">
+    <div class="header-info">
+      <h1>Blog</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "BlogHeader",
+};
+</script>
+
+<style lang="scss">
+.header {
+  position: relative;
+  height: 350px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url("../assets/images/bg.jpeg") no-repeat center center;
+  background-size: cover;
+  
+  &::before{
+    position: absolute;
+    content: '';
+    top:0;
+    left:0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(60deg, rgb(0 142 133), rgb(0 142 133 / 36%));
+  }
+  .header-info {
+    z-index: 5;
+    h1 {
+      color: #fff;
+    }
+  }
+}
+</style>
