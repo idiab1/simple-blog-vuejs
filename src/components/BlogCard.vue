c1db5eddd85de048c5a85757a28b6d80a53ce4ce
islamdiab-stack <islamdiab967@gmail.com>
2021-05-09 01:09:42 +0200

    Create blog card component for blog artical

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="blog-card">
+    <div class="card-image">
+      <img src="../assets/images/blog-post.jpg" alt="Card Title" />
+    </div>
+    <div class="card-content">
+      <h2 class="card-title">Card Title</h2>
+      <p class="summary">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quia
+        porro maxime facere magnam accusantium autem dolorem quibusdam, minus
+        ducimus dolor optio numquam, fugit illum!
+      </p>
+      <a href="#">Show More</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogCard",
+};
+</script>
+
+<style lang="scss"></style>
