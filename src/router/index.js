bf26ad95264e957763c0d142bf6ab458cfd068e3
islamdiab-stack <islamdiab967@gmail.com>
2021-05-14 10:51:03 +0200

    Create new route for single blog [blog details] and add parameter in this route for id

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 
 import {createRouter, createWebHistory} from 'vue-router';
 import Home from "../views/Home.vue";
+import SingleBlog from "../views/SingleBlog.vue";
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/blog/:id',
+    name: "SingleBlog",
+    component: SingleBlog,
   }
 ];
 
